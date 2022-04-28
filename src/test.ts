fadb18597007271e762fb8c06a1785df41851754
Frank <frank@ifrank.cn>
2022-04-28 17:16:15 +0800

    add Dockerfile

--- src/test.ts
+++ src/test.ts
@@ -4,19 +4,23 @@ interface Config {}
 
 export default class TestPlugin {
   name = 'TestPlugin'
-  logger: Logger
+  private readonly logger: Logger
 
   constructor(private ctx: Context, private config: Config) {
     this.logger = ctx.logger(this.name)
     ctx.middleware(this.callback.bind(this))
   }
 
-  callback(session: Session, next: Next) {
-    if (process.env.NODE_ENV === 'development') {
-      this.logger.info('msg', session.content)
-      this.logger.info('ids', session.userId, session.username, session.guildId)
-      this.logger.info('user', session.user) // 对应 "user" model
-    }
+  async callback(session: Session, next: Next) {
+    if (
+      process.env.NODE_ENV !== 'development' &&
+      process.env.NODE_ENV !== 'debug' &&
+      (session.userId !== '956541711' || session.guildId !== '849791643')
+    )
+      return next()
+
+    this.logger.info(JSON.stringify(session.content))
+
     return next()
   }
 }
